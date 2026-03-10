8386d544b340de12fe183f8fdd7b49a44d09d132
User Yana <ruavky4@yandex.ru>
2026-03-10 22:10:06 +0300

    поправила Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -8,30 +8,14 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Интерфейс хранилища фильмов.
- */
 public interface FilmStorage {
 
-    /**
-     * Возвращает все фильмы.
-     * @return коллекция фильмов
-     */
 
     Collection<Film> findAll();
 
-    /**
-     * Возвращает фильм по идентификатору.
-     * @param id идентификатор фильма
-     * @return Optional с фильмом
-     */
+
     Optional<Film> findById(Long id);
 
-    /**
-     * Добавляет новый фильм.
-     * @param film фильм для добавления
-     * @return сохранённый фильм с установленным id
-     */
     Film create(Film film);
 
     /**
