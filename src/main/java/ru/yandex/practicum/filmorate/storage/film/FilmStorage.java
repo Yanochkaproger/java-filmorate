313f3382ec7e8522d1bf7a50cb1b1c6ef0bd5f1e
User Yana <ruavky4@yandex.ru>
2026-03-10 21:16:53 +0300

    style: исправить Checkstyle ошибки в интерфейсах

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -3,18 +3,72 @@ package ru.yandex.practicum.filmorate.storage.film;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.MpaRating;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Интерфейс хранилища фильмов.
+ */
 public interface FilmStorage {
+
+    /**
+     * Возвращает все фильмы.
+     * @return коллекция фильмов
+     */
     Collection<Film> findAll();
+
+    /**
+     * Возвращает фильм по идентификатору.
+     * @param id идентификатор фильма
+     * @return Optional с фильмом
+     */
     Optional<Film> findById(Long id);
+
+    /**
+     * Добавляет новый фильм.
+     * @param film фильм для добавления
+     * @return сохранённый фильм с установленным id
+     */
     Film create(Film film);
+
+    /**
+     * Обновляет существующий фильм.
+     * @param film фильм для обновления
+     * @return обновлённый фильм
+     */
     Film update(Film film);
+
+    /**
+     * Удаляет фильм по идентификатору.
+     * @param id идентификатор фильма
+     */
     void delete(Long id);
+
+    /**
+     * Возвращает все жанры.
+     * @return список жанров
+     */
     List<Genre> findAllGenres();
+
+    /**
+     * Возвращает жанр по идентификатору.
+     * @param id идентификатор жанра
+     * @return Optional с жанром
+     */
     Optional<Genre> findGenreById(Long id);
+
+    /**
+     * Возвращает все рейтинги MPA.
+     * @return список рейтингов
+     */
     List<MpaRating> findAllMpaRatings();
+
+    /**
+     * Возвращает рейтинг MPA по идентификатору.
+     * @param id идентификатор рейтинга
+     * @return Optional с рейтингом
+     */
     Optional<MpaRating> findMpaRatingById(Long id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,13 +1,45 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.Collection;
 import java.util.Optional;
 
+/**
+ * Интерфейс хранилища пользователей.
+ */
 public interface UserStorage {
+
+    /**
+     * Возвращает всех пользователей.
+     * @return коллекция пользователей
+     */
     Collection<User> findAll();
+
+    /**
+     * Возвращает пользователя по идентификатору.
+     * @param id идентификатор пользователя
+     * @return Optional с пользователем
+     */
     Optional<User> findById(Long id);
+
+    /**
+     * Добавляет нового пользователя.
+     * @param user пользователь для добавления
+     * @return сохранённый пользователь с установленным id
+     */
     User create(User user);
+
+    /**
+     * Обновляет существующего пользователя.
+     * @param user пользователь для обновления
+     * @return обновлённый пользователь
+     */
     User update(User user);
+
+    /**
+     * Удаляет пользователя по идентификатору.
+     * @param id идентификатор пользователя
+     */
     void delete(Long id);
 }
